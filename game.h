f71a3f66186d1469ef64b16d77925297f364fd1c
24020196 <24020196@vnu.edu.vn>
2025-03-06 22:12:20 +0700

    Add files via upload

--- game.h
+++ game.h
@@ -1,6 +1,7 @@
+
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
-void loopgame(SDL_Window* window,SDL_Renderer* renderer,int dokho);
+void loopgame(SDL_Window* window,SDL_Renderer* renderer,int &dokho);
 
 
 #endif // GAME_H_INCLUDED
--- menu.h
+++ menu.h
@@ -1,6 +1,6 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
 
-void menuevent(SDL_Window* window,SDL_Renderer* renderer);
+
 void renmenu(SDL_Window* window,SDL_Renderer* renderer);
 #endif // MENU_H_INCLUDED
