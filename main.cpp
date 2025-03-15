60596aa68f9c6a1e5d2f8ec427ca09abc8e0026d
24020196 <24020196@vnu.edu.vn>
2025-03-15 23:58:21 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,14 +17,26 @@ int main(int argc, char* argv[])
     initSDL(window, renderer);
 
     while(true){
-        renmenu(window,renderer);
+        SDL_Init(SDL_INIT_AUDIO);
+        SDL_AudioSpec wavSpec;
+        Uint32 wavLength;
+        Uint8 *wavBuffer;
+        SDL_LoadWAV("music.wav", &wavSpec, &wavBuffer, &wavLength);
+        SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+        SDL_QueueAudio(deviceId, wavBuffer, wavLength);
+        SDL_PauseAudioDevice(deviceId, 0);
 
+        renmenu(window,renderer);
         int dokho;
         ifstream file("temp.txt");
         file>>dokho;
         file.close();
         loopgame(window,renderer,dokho);
 
+        SDL_PauseAudioDevice(deviceId,1);
+        SDL_CloseAudioDevice(deviceId);
+        SDL_FreeWAV(wavBuffer);
     }
+
     return 0;
 }
