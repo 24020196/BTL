34d344fb387e33dce1965beace7e9f9ddf308619
24020196 <24020196@vnu.edu.vn>
2025-03-15 23:56:53 +0700

    Add files via upload

--- khoitao.cpp
+++ khoitao.cpp
@@ -28,6 +28,8 @@ void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
     if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
     SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+
 }
 
 void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
